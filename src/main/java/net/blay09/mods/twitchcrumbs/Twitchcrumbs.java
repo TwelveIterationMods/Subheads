508f1adeb2c3e6f76a41987312ca6704c906385e
GoryMoon <gurreja@gmail.com>
2018-06-18 21:31:30 +0200

    Ported to 1.12 and added tab complete for reload command

--- src/main/java/net/blay09/mods/twitchcrumbs/Twitchcrumbs.java
+++ src/main/java/net/blay09/mods/twitchcrumbs/Twitchcrumbs.java
@@ -5,6 +5,7 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -18,6 +19,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -66,31 +68,36 @@ public class Twitchcrumbs {
 	public void serverStarting(FMLServerStartingEvent event) {
 		event.registerServerCommand(new CommandBase() {
 			@Override
-			public String getCommandName() {
+			public String getName() {
 				return "twitchcrumbs";
 			}
 
 			@Override
-			public String getCommandUsage(ICommandSender sender) {
+			public String getUsage(ICommandSender sender) {
 				return "/twitchcrumbs reload";
 			}
 
 			@Override
 			public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 				if (args.length != 1) {
-					throw new WrongUsageException(getCommandUsage(sender));
+					throw new WrongUsageException(getUsage(sender));
 				}
 				if (args[0].equals("reload")) {
 					int registered = reloadTwitchCrumbs();
-					sender.addChatMessage(new TextComponentString("Reloaded Twitchcrumbs - registered " + registered + " users."));
+					sender.sendMessage(new TextComponentString("Reloaded Twitchcrumbs - registered " + registered + " users."));
 					return;
 				}
-				throw new WrongUsageException(getCommandUsage(sender));
+				throw new WrongUsageException(getUsage(sender));
 			}
 
 			@Override
 			public int getRequiredPermissionLevel() {
 				return 2;
+			}
+
+			@Override
+			public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+				return args.length == 1 ? Collections.singletonList("reload"): super.getTabCompletions(server, sender, args, targetPos);
 			}
 		});
 	}
