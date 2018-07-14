a0afa850c0c5dc6dc9f3ae00714c0349de930700
blay09 <blay09@hotmail.de>
2018-07-15 01:07:25 +0200

    Fix loaded users number including non-twitchcrumbs users from the Headcrumbs "others" config Fix users not being loaded when no cache is present

--- src/main/java/net/blay09/mods/twitchcrumbs/CachedAPI.java
+++ src/main/java/net/blay09/mods/twitchcrumbs/CachedAPI.java
@@ -7,53 +7,54 @@ import java.net.URL;
 
 public class CachedAPI {
 
-	private static final long DEFAULT_CACHE_TIME = 1000*60*60*24;
+    private static final long DEFAULT_CACHE_TIME = 1000 * 60 * 60 * 24;
 
-	public static InputStream loadCachedAPI(String url, String fileName) throws IOException {
-		return loadCachedAPI(url, fileName, DEFAULT_CACHE_TIME);
-	}
+    public static InputStream loadCachedAPI(String url, String fileName) throws IOException {
+        return loadCachedAPI(url, fileName, DEFAULT_CACHE_TIME);
+    }
 
-	public static InputStream loadCachedAPI(String url, String fileName, long maxCacheTime) throws IOException {
-		return loadCachedAPI(url, new File(getCacheDirectory(), fileName), maxCacheTime);
-	}
+    public static InputStream loadCachedAPI(String url, String fileName, long maxCacheTime) throws IOException {
+        return loadCachedAPI(url, new File(getCacheDirectory(), fileName), maxCacheTime);
+    }
 
-	public static InputStream loadCachedAPI(String url, File cacheFile, long maxCacheTime) throws IOException {
-		InputStream in = loadLocal(cacheFile, false, maxCacheTime);
-		if(in == null) {
-			in = loadRemote(url);
-			if(in == null) {
-				in = loadLocal(cacheFile, true, maxCacheTime);
-				if(in == null) {
-					throw new IOException("Could not grab remote source and no local cache present.");
-				}
-			} else {
-				try {
-					IOUtils.copy(in, new FileOutputStream(cacheFile));
-				} catch (IOException e) {
-					Twitchcrumbs.logger.error("Failed to cache {}: {}", url, e);
-				}
-			}
-		}
-		return in;
-	}
+    public static InputStream loadCachedAPI(String url, File cacheFile, long maxCacheTime) throws IOException {
+        InputStream in = loadLocal(cacheFile, false, maxCacheTime);
+        if (in == null) {
+            in = loadRemote(url);
+            if (in != null) {
+                try {
+                    IOUtils.copy(in, new FileOutputStream(cacheFile));
+                } catch (IOException e) {
+                    Twitchcrumbs.logger.error("Failed to cache {}: {}", url, e);
+                }
+            }
 
-	private static InputStream loadLocal(File file, boolean force, long maxCacheTime) throws IOException {
-		if(file.exists() && (force || System.currentTimeMillis() - file.lastModified() < maxCacheTime)) {
-			return new FileInputStream(file);
-		}
-		return null;
-	}
+            in = loadLocal(cacheFile, true, maxCacheTime);
+            if (in == null) {
+                throw new IOException("Could not grab remote source and no local cache present.");
+            }
+        }
 
-	private static InputStream loadRemote(String url) throws IOException {
-		URL apiURL = new URL(url);
-		return apiURL.openStream();
-	}
+        return in;
+    }
 
-	public static File getCacheDirectory() {
-		File file = new File("twitchcrumbs-cache/");
-		//noinspection ResultOfMethodCallIgnored
-		file.mkdirs();
-		return file;
-	}
+    private static InputStream loadLocal(File file, boolean force, long maxCacheTime) throws IOException {
+        if (file.exists() && (force || System.currentTimeMillis() - file.lastModified() < maxCacheTime)) {
+            return new FileInputStream(file);
+        }
+        return null;
+    }
+
+    private static InputStream loadRemote(String url) throws IOException {
+        URL apiURL = new URL(url);
+        return apiURL.openStream();
+    }
+
+    public static File getCacheDirectory() {
+        File file = new File("twitchcrumbs-cache/");
+        //noinspection ResultOfMethodCallIgnored
+        file.mkdirs();
+        return file;
+    }
 
 }
--- src/main/java/net/blay09/mods/twitchcrumbs/Twitchcrumbs.java
+++ src/main/java/net/blay09/mods/twitchcrumbs/Twitchcrumbs.java
@@ -34,160 +34,167 @@ import java.util.function.Consumer;
 @Mod(modid = "twitchcrumbs", name = "Twitchcrumbs", dependencies = "required-after:headcrumbs")
 public class Twitchcrumbs {
 
-	public static final Logger logger = LogManager.getLogger();
+    public static final Logger logger = LogManager.getLogger();
 
-	@Mod.Instance
-	public static Twitchcrumbs instance;
+    @Mod.Instance
+    public static Twitchcrumbs instance;
 
-	private final List<String> whitelists = new ArrayList<>();
-	private boolean autoReload;
-	private int cacheTime;
-	private int reloadInterval;
-	private boolean firstTick = true;
+    private final List<String> whitelists = new ArrayList<>();
+    private boolean autoReload;
+    private int cacheTime;
+    private int reloadInterval;
+    private boolean firstTick = true;
 
-	private Thread thread;
+    private Thread thread;
 
-	private String[] originalNames;
-	private int tickTimer;
+    private String[] originalNames;
+    private int tickTimer;
 
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		String[] sources = config.getStringList("sources", "general", new String[0], "One whitelist source link per line. Example: http://whitelist.twitchapps.com/list.php?id=12345");
-		Collections.addAll(whitelists, sources);
-		cacheTime = config.getInt("cacheTime", "general", 60 * 60 * 24, 0, Integer.MAX_VALUE, "How long should the cache be used until updates are pulled? (if autoReload is false) (in seconds)");
-		autoReload = config.getBoolean("autoReload", "general", false, "Should the Twitchcrumbs automatically be reloaded in a specific interval? This will mean reading the remote file again and will reset Headcrumb's already-spawned list. The Creative Tab and NEI won't be updated until the game restarts, though.");
-		reloadInterval = config.getInt("reloadInterval", "general", 60, 10, 60 * 12, "If autoReload is enabled, at what interval in minutes should the reload happen? (approximately, based on TPS)") * 60 * 20;
-		config.save();
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+        String[] sources = config.getStringList("sources", "general", new String[0], "One whitelist source link per line. Example: http://whitelist.twitchapps.com/list.php?id=12345");
+        Collections.addAll(whitelists, sources);
+        cacheTime = config.getInt("cacheTime", "general", 60 * 60 * 24, 0, Integer.MAX_VALUE, "How long should the cache be used until updates are pulled? (if autoReload is false) (in seconds)");
+        autoReload = config.getBoolean("autoReload", "general", false, "Should the Twitchcrumbs automatically be reloaded in a specific interval? This will mean reading the remote file again and will reset Headcrumb's already-spawned list. The Creative Tab and NEI won't be updated until the game restarts, though.");
+        reloadInterval = config.getInt("reloadInterval", "general", 60, 10, 60 * 12, "If autoReload is enabled, at what interval in minutes should the reload happen? (approximately, based on TPS)") * 60 * 20;
+        config.save();
 
-		FMLCommonHandler.instance().bus().register(this);
-	}
+        FMLCommonHandler.instance().bus().register(this);
+    }
 
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
-	}
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-	}
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    }
 
-	@Mod.EventHandler
-	public void serverStarting(FMLServerStartingEvent event) {
-		event.registerServerCommand(new CommandBase() {
-			@Override
-			public String getName() {
-				return "twitchcrumbs";
-			}
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        event.registerServerCommand(new CommandBase() {
+            @Override
+            public String getName() {
+                return "twitchcrumbs";
+            }
 
-			@Override
-			public String getUsage(ICommandSender sender) {
-				return "/twitchcrumbs reload";
-			}
+            @Override
+            public String getUsage(ICommandSender sender) {
+                return "/twitchcrumbs reload";
+            }
 
-			@Override
-			public void execute(MinecraftServer server, final ICommandSender sender, String[] args) throws CommandException {
-				if (args.length != 1) {
-					throw new WrongUsageException(getUsage(sender));
-				}
-				if (args[0].equals("reload")) {
-					sender.sendMessage(new TextComponentString("Reloading Twitchcrumbs"));
-					reloadTwitchCrumbs(registered -> sender.sendMessage(new TextComponentString("Reloaded Twitchcrumbs - registered " + registered + " users.")));
-					return;
-				}
-				throw new WrongUsageException(getUsage(sender));
-			}
+            @Override
+            public void execute(MinecraftServer server, final ICommandSender sender, String[] args) throws CommandException {
+                if (args.length != 1) {
+                    throw new WrongUsageException(getUsage(sender));
+                }
+                if (args[0].equals("reload")) {
+                    sender.sendMessage(new TextComponentString("Reloading Twitchcrumbs..."));
+                    reloadTwitchCrumbs(registered -> sender.sendMessage(new TextComponentString("Reloaded Twitchcrumbs - registered " + registered + " users.")));
+                    return;
+                }
+                throw new WrongUsageException(getUsage(sender));
+            }
 
-			@Override
-			public int getRequiredPermissionLevel() {
-				return 2;
-			}
+            @Override
+            public int getRequiredPermissionLevel() {
+                return 2;
+            }
 
-			@Override
-			public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
-				return args.length == 1 ? Collections.singletonList("reload"): super.getTabCompletions(server, sender, args, targetPos);
-			}
-		});
-	}
+            @Override
+            public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+                return args.length == 1 ? Collections.singletonList("reload") : super.getTabCompletions(server, sender, args, targetPos);
+            }
+        });
+    }
 
-	@SubscribeEvent
-	public void tick(TickEvent.ServerTickEvent event) {
-		if (firstTick) { // We do this here instead of in init to forcefully skip Headcrumb's initialization code. Creating all the head stacks, adding all the dungeon loot and all that other stuff is too much for huge lists like SF2.5.
-			firstTick = false;
-			reloadTwitchCrumbs(null);
-		}
-		if (autoReload) {
-			tickTimer++;
-			if (tickTimer > reloadInterval) {
-				tickTimer = 0;
-				reloadTwitchCrumbs(null);
-			}
-		}
-	}
+    @SubscribeEvent
+    public void tick(TickEvent.ServerTickEvent event) {
+        if (firstTick) { // We do this here instead of in init to forcefully skip Headcrumb's initialization code. Creating all the head stacks, adding all the dungeon loot and all that other stuff is too much for huge lists like SF2.5.
+            firstTick = false;
+            reloadTwitchCrumbs(null);
+        }
 
-	@SuppressWarnings("unchecked")
-	public void reloadTwitchCrumbs(Consumer<Integer> reloadDone) {
-		if (thread != null) return;
-		thread = new Thread(() -> {
-			// Load the whitelist from all sources
-			List<String> list = new ArrayList<>();
-			for (String source : whitelists) {
-				try (BufferedReader reader = new BufferedReader(new InputStreamReader(CachedAPI.loadCachedAPI(source, source.replace(":", "_").replace("/", "_").replace("?", "_"), 1000 * cacheTime)))) {
-					String line;
-					while ((line = reader.readLine()) != null) {
-						list.add(line);
-					}
-				} catch (IOException e) {
-					logger.error("Failed to load whitelist from source {}: {}", source, e);
-				}
-			}
-			Runnable update = () -> {
-				int reg = updateData(list);
-				if (reloadDone != null) {
-					reloadDone.accept(reg);
-				}
-				thread = null;
-			};
-			if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-				FMLClientHandler.instance().getClient().addScheduledTask(update);
-			} else {
-				FMLCommonHandler.instance().getMinecraftServerInstance().addScheduledTask(update);
-			}
-		});
-		thread.start();
-	}
+        if (autoReload) {
+            tickTimer++;
+            if (tickTimer > reloadInterval) {
+                tickTimer = 0;
+                reloadTwitchCrumbs(null);
+            }
+        }
+    }
 
-	private int updateData(List<String> list) {
-		logger.info("Registering {} Twitchcrumbs users...", list.size());
-		// We don't use Headcrumb's IMC API because it's inefficient and requires mod-sent option to be enabled
-		// Append our whitelist names to the "others" list in Headcrumbs instead
-		try {
-			Class headcrumbs = Class.forName("ganymedes01.headcrumbs.Headcrumbs");
-			// ## Option was removed in 1.9.4:
-//			if (list.size() > 50) { // Calm down now.
-//				Field hidePlayerHeadsFromTabField = headcrumbs.getField("hidePlayerHeadsFromTab");
-//				hidePlayerHeadsFromTabField.set(null, true);
-//			}
-			Field othersField = headcrumbs.getField("others");
-			String[] others = (String[]) othersField.get(null);
-			if (originalNames == null) {
-				originalNames = new String[others.length];
-				System.arraycopy(others, 0, originalNames, 0, others.length);
-			} else {
-				others = originalNames;
-			}
-			Collections.addAll(list, others);
-			othersField.set(null, list.toArray(new String[list.size()]));
+    @SuppressWarnings("unchecked")
+    public void reloadTwitchCrumbs(Consumer<Integer> reloadDone) {
+        if (thread != null) {
+            return;
+        }
 
-			// Clear EntityHuman's name cache to allow immediate spawning of new names in case of a reload
-			Class entityHuman = Class.forName("ganymedes01.headcrumbs.entity.EntityHuman");
-			Field namesField = entityHuman.getDeclaredField("names");
-			namesField.setAccessible(true);
-			List<String> names = (List<String>) namesField.get(null);
-			names.clear();
-		} catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException | ClassCastException e) {
-			logger.error("Oops! Twitchcrumbs is not compatible with this version of Headcrumbs!", e);
-		}
-		return list.size();
-	}
+        thread = new Thread(() -> {
+            // Load the whitelist from all sources
+            List<String> list = new ArrayList<>();
+            for (String source : whitelists) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(CachedAPI.loadCachedAPI(source, source.replace(":", "_").replace("/", "_").replace("?", "_"), 1000 * cacheTime)))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        list.add(line);
+                    }
+                } catch (IOException e) {
+                    logger.error("Failed to load whitelist from source {}: {}", source, e);
+                }
+            }
+
+            Runnable update = () -> {
+                // Grab the count here as the list will be modified in updateData
+                int count = list.size();
+
+                updateData(list);
+
+                if (reloadDone != null) {
+                    reloadDone.accept(count);
+                }
+
+                thread = null;
+            };
+
+            if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
+                FMLClientHandler.instance().getClient().addScheduledTask(update);
+            } else {
+                FMLCommonHandler.instance().getMinecraftServerInstance().addScheduledTask(update);
+            }
+        });
+
+        thread.start();
+    }
+
+    private void updateData(List<String> list) {
+        logger.info("Registering {} Twitchcrumbs users...", list.size());
+        // We don't use Headcrumb's IMC API because it's inefficient and requires mod-sent option to be enabled
+        // Append our whitelist names to the "others" list in Headcrumbs instead
+        try {
+            Class headcrumbs = Class.forName("ganymedes01.headcrumbs.Headcrumbs");
+            Field othersField = headcrumbs.getField("others");
+            String[] others = (String[]) othersField.get(null);
+            if (originalNames == null) {
+                originalNames = new String[others.length];
+                System.arraycopy(others, 0, originalNames, 0, others.length);
+            } else {
+                others = originalNames;
+            }
+
+            Collections.addAll(list, others);
+            othersField.set(null, list.toArray(new String[0]));
+
+            // Clear EntityHuman's name cache to allow immediate spawning of new names in case of a reload
+            Class entityHuman = Class.forName("ganymedes01.headcrumbs.entity.EntityHuman");
+            Field namesField = entityHuman.getDeclaredField("names");
+            namesField.setAccessible(true);
+            List<String> names = (List<String>) namesField.get(null);
+            names.clear();
+        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException | ClassCastException e) {
+            logger.error("Oops! Twitchcrumbs is not compatible with this version of Headcrumbs!", e);
+        }
+    }
 
 }
